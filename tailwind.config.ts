beec77ac025a7039ca4d0763a2a882204391148c
Ken <johnkennethvelano@gmail.com>
2025-02-09 02:53:56 +0800

    Add husky and set up tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,30 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // background: "var(--background)",
+        // foreground: "var(--foreground)",
+        colors: {
+          primary: '#591DA9',
+          secondary: {
+            DEFAULT: '#051960',
+            900: '#00020A',  
+            800: '#030D30',  
+          },
+          accent: {
+            100: '#75A3EB', 
+            200: '#CB98ED', 
+          },
+          gray: '#C0B4D0',
+          text: '#120622',
+          background: {
+            light: '#F8F7FC',  
+            dark: '#090311',   
+          },
+          nav: {
+            click: "rgba(117, 163, 235, 0.5)", 
+            hover: "rgba(117, 163, 235, 0.2)"
+          }
+        },
       },
     },
   },
