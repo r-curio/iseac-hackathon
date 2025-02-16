4aacc782830cbd98b495ec460c20075bd3e48224
Ken <johnkennethvelano@gmail.com>
2025-02-16 13:54:16 +0800

    merge

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { useSidebar } from "../../hooks/use-sidebar";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 const RouteLayout = ({ children }: { children: React.ReactNode }) => {
+  const sidebar = useSidebar();
+  const isMobile = useIsMobile();
+
   return (
-    <div className="flex h-screen items-center justify-center overflow-auto py-4">
+    <div
+      className="m-0 mt-16 flex items-center justify-center p-0 lg:m-0 lg:ml-auto lg:px-10 lg:py-8"
+      style={{
+        width: isMobile
+          ? "100%"
+          : sidebar.isOpen
+            ? "calc(100% - 240px)"
+            : "calc(100% - 80px)",
+      }}
+    >
       {children}
     </div>
   );
---
+++ src/lib/utils.ts
@@ -0,0 +1,63 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const glaresPositions = [
+  {
+    left: "0%",
+    top: "0%",
+    delay: "0s",
+    duration: "5s",
+  },
+  {
+    left: "25%",
+    top: "15%",
+    delay: "0.5s",
+    duration: "7s",
+  },
+  {
+    left: "75%",
+    top: "20%",
+    delay: "1.2s",
+    duration: "6s",
+  },
+  {
+    left: "90%",
+    top: "45%",
+    delay: "0.8s",
+    duration: "8s",
+  },
+  {
+    left: "60%",
+    top: "75%",
+    delay: "1.5s",
+    duration: "5.5s",
+  },
+  {
+    left: "10%",
+    top: "85%",
+    delay: "2s",
+    duration: "7.5s",
+  },
+  {
+    left: "45%",
+    top: "35%",
+    delay: "0.3s",
+    duration: "6.5s",
+  },
+  {
+    left: "80%",
+    top: "5%",
+    delay: "1.8s",
+    duration: "4.5s",
+  },
+  {
+    left: "35%",
+    top: "95%",
+    delay: "1s",
+    duration: "6.8s",
+  },
+];
