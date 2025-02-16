f4a4432fde85beacb6864203c62e7ef08a4d4231
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2025-02-16 13:33:40 +0800

    refactor: remove sidebar width adjustment from layout component

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,16 +1,11 @@
 "use client";
 import React from "react";
-import { useSidebar } from "../../hooks/use-sidebar";
 
 const RouteLayout = ({ children }: { children: React.ReactNode }) => {
-  const sidebar = useSidebar();
 
   return (
     <div
       className="ml-auto flex items-center justify-center px-10 py-8"
-      style={{
-        width: sidebar.isOpen ? "calc(100% - 170px)" : "calc(100% - 10px)",
-      }}
     >
       {children}
     </div>
