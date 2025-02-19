c9b7659056539f4a39f36cfa6e8f2b427f77d316
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2025-02-19 18:51:04 +0800

    feat: add FlashcardArray component for navigating through flashcards

---
+++ src/components/flashcardArray.tsx
@@ -0,0 +1,52 @@
+'use client'
+import { useState } from "react";
+import Flashcard from "./flashcard";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { Progress } from "./ui/progress";
+
+interface FlashcardArrayProps {
+    flashcard: {
+        front: string;
+        back: string;
+    }[];
+    isFlashcard: boolean;
+}
+
+export default function FlashcardArray({flashcard, isFlashcard} : FlashcardArrayProps) {
+
+    const [currentCard, setCurrentCard] = useState(0);
+
+    const handleNextCard = () => {
+        if (currentCard === flashcard.length - 1) {
+            return;
+        }
+        setCurrentCard((prev) => prev + 1);
+    }
+
+    const handlePrevCard = () => {
+        if (currentCard === 0) {
+            return;
+        }
+        setCurrentCard((prev) => prev - 1);
+    }
+
+    return (
+        <div className='mt-8'>
+            {isFlashcard && <Progress value={33} />}
+            <div className='flex items-center justify-center mt-4 h-[480px]'>
+                <Flashcard front={flashcard[currentCard].front} back={flashcard[currentCard].back}/>
+            </div>
+            <div>
+                <div className="flex items-center justify-center gap-12 mt-8">
+                    <button className="flex items-center justify-center px-7 py-2 border-[#CB98ED] rounded-xl border" onClick={handlePrevCard}>
+                        <ArrowLeft size={24} color='#CB98ED'/>
+                    </button>
+                    <h1 className='font-semibold text-xl '> {currentCard + 1 } / {flashcard.length}</h1>
+                    <button className="flex items-center justify-center px-7 py-2 border-[#CB98ED] rounded-xl border" onClick={handleNextCard}>
+                        <ArrowRight size={24} color='#CB98ED'/>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
