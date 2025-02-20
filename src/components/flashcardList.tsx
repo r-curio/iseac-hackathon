4d8cc9e295ea689dee32d81aec53288c06bc0b22
Ken <johnkennethvelano@gmail.com>
2025-02-20 20:40:35 +0800

    add flashcard modal

--- src/components/flashcardList.tsx
+++ src/components/flashcardList.tsx
@@ -1,120 +1,149 @@
-'use client'
+"use client";
 import { useState } from "react";
-import { Pencil, Check } from "lucide-react";
+import { Pencil, Check, PlusIcon } from "lucide-react";
 import { Textarea } from "./ui/modified-textarea";
+import { Button } from "./ui/button";
+import AddCardModal from "./modals/add-card-modal";
 
 interface flashcardListProps {
-    flashcards: {
-        id: string;
-        front: string;
-        back: string;
-    }[];
-    handleFlashcardsChange: (flashcards: flashcard[]) => void;
-}
-
-interface flashcard {
+  flashcards: {
     id: string;
     front: string;
     back: string;
+  }[];
+  handleFlashcardsChange: (flashcards: flashcard[]) => void;
 }
 
-const FlashcardComponent = (props: { flashcard: flashcard, handleFlashcardChange: (flashcard: flashcard) => void }) => {
-    const [isEditing, setIsEditing] = useState(false);
-    const [front, setFront] = useState(props.flashcard.front);
-    const [back, setBack] = useState(props.flashcard.back);
-
-    const handleSave = () => {
-        setIsEditing(false);
-
-        try {
-            fetch('/api/flashcard', {
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                method: 'PUT',
-                body: JSON.stringify({
-                    id: props.flashcard.id,
-                    front,
-                    back,
-                    type: 'update'
-                })
-            })
-        } catch (error) {
-            console.error('An error occurred:', error)
-            return;
-        }
-
-        props.handleFlashcardChange({
-            id: props.flashcard.id,
-            front,
-            back
-        });
-
-    };
-
-    return (
-        <div className="bg-flashcard-gradient text-white p-7 rounded-xl border-[#591DA9] border-2 flex min-h-[110px] justify-between">
-            <div className="flex flex-1">
-                <div className="pr-4 border-r-2 border-white max-w-[300px] min-w-[300px] text-wrap">
-                {isEditing ? (
-                        <Textarea
-                            value={front}
-                            onChange={(e) => setFront(e.target.value)}
-                            className="bg-transparent text-white border-white/20 resize-none text-xl font-normal w-full p-0 border-none focus:ring-0 focus-visible:ring-0 whitespace-pre-wrap break-words"
-                            placeholder="Front of flashcard"
-                        />
-                    ) : (
-                        <h1 className="text-xl font-normal text-wrap break-words">{front}</h1>
-                    )}
-                </div>
-                <div className="pl-4 font-normal text-xl flex-1">
-                    {isEditing ? (
-                        <Textarea
-                            value={back}
-                            onChange={(e) => setBack(e.target.value)}
-                            className="bg-transparent text-white border-white/20 resize-none h-full text-xl font-normal w-full p-0 border-none focus:ring-0 focus-visible:ring-0"
-                            placeholder="Back of flashcard"
-                        />
-                    ) : (
-                        <p>{back}</p>
-                    )}
-                </div>
-            </div>
-            <div>
-                <button 
-                    onClick={() => isEditing ? handleSave() : setIsEditing(true)}
-                    className="hover:opacity-80 transition-opacity"
-                >
-                    {isEditing ? (
-                        <Check size={24} />
-                    ) : (
-                        <Pencil size={24} />
-                    )}
-                </button>
-            </div>
-        </div>
-    )
+interface flashcard {
+  id: string;
+  front: string;
+  back: string;
 }
 
-export default function FlashcardList({ flashcards, handleFlashcardsChange }: flashcardListProps) {
+const FlashcardComponent = (props: {
+  flashcard: flashcard;
+  handleFlashcardChange: (flashcard: flashcard) => void;
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [front, setFront] = useState(props.flashcard.front);
+  const [back, setBack] = useState(props.flashcard.back);
 
-    const handleFlashcardChange = (flashcard: flashcard) => {
-        const updatedFlashcards = flashcards.map((f) => {
-            if (f.id === flashcard.id) {
-                return flashcard;
-            }
-            return f;
-        });
+  const handleSave = () => {
+    setIsEditing(false);
 
-        handleFlashcardsChange(updatedFlashcards);
+    try {
+      fetch("/api/flashcard", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "PUT",
+        body: JSON.stringify({
+          id: props.flashcard.id,
+          front,
+          back,
+          type: "update",
+        }),
+      });
+    } catch (error) {
+      console.error("An error occurred:", error);
+      return;
     }
 
-    return (
-        <div className="w-full mt-7 py-[42px] px-[80px] bg-[#0C101780]/50 space-y-8">
-            <h1 className="text-2xl font-normal mb-4">Flashcards</h1>
-            {flashcards.map((flashcard, index) => (
-                <FlashcardComponent key={index} flashcard={flashcard} handleFlashcardChange={handleFlashcardChange}/>
-            ))}
+    props.handleFlashcardChange({
+      id: props.flashcard.id,
+      front,
+      back,
+    });
+  };
+
+  return (
+    <div className="flex min-h-[110px] justify-between rounded-xl border-2 border-[#591DA9] bg-flashcard-gradient p-7 text-white">
+      <div className="flex flex-1">
+        <div className="min-w-[300px] max-w-[300px] text-wrap border-r-2 border-white pr-4">
+          {isEditing ? (
+            <Textarea
+              value={front}
+              onChange={(e) => setFront(e.target.value)}
+              className="w-full resize-none whitespace-pre-wrap break-words border-none border-white/20 bg-transparent p-0 text-xl font-normal text-white focus:ring-0 focus-visible:ring-0"
+              placeholder="Front of flashcard"
+            />
+          ) : (
+            <h1 className="text-wrap break-words text-xl font-normal">
+              {front}
+            </h1>
+          )}
         </div>
-    )
+        <div className="flex-1 pl-4 text-xl font-normal">
+          {isEditing ? (
+            <Textarea
+              value={back}
+              onChange={(e) => setBack(e.target.value)}
+              className="h-full w-full resize-none border-none border-white/20 bg-transparent p-0 text-xl font-normal text-white focus:ring-0 focus-visible:ring-0"
+              placeholder="Back of flashcard"
+            />
+          ) : (
+            <p>{back}</p>
+          )}
+        </div>
+      </div>
+      <div>
+        <button
+          onClick={() => (isEditing ? handleSave() : setIsEditing(true))}
+          className="transition-opacity hover:opacity-80"
+        >
+          {isEditing ? <Check size={24} /> : <Pencil size={24} />}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default function FlashcardList({
+  flashcards,
+  handleFlashcardsChange,
+}: flashcardListProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleFlashcardChange = (flashcard: flashcard) => {
+    const updatedFlashcards = flashcards.map((f) => {
+      if (f.id === flashcard.id) {
+        return flashcard;
+      }
+      return f;
+    });
+
+    handleFlashcardsChange(updatedFlashcards);
+  };
+
+  return (
+    <>
+      <AddCardModal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        loading={isLoading}
+        setLoading={setIsLoading}
+      />
+      <div className="mt-7 w-full space-y-8 bg-[#0C101780]/50 px-[80px] py-[42px]">
+        <div className="flex w-full items-center justify-between">
+          <h1 className="mb-4 text-2xl font-normal">Flashcards</h1>
+          <Button
+            variant={"outline"}
+            onClick={() => setIsOpen(true)}
+            className="rounded-xl bg-transparent p-6"
+          >
+            <PlusIcon size={24} />
+            <p>Add Flashcard</p>
+          </Button>
+        </div>
+        {flashcards.map((flashcard, index) => (
+          <FlashcardComponent
+            key={index}
+            flashcard={flashcard}
+            handleFlashcardChange={handleFlashcardChange}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
---
+++ src/components/modals/add-card-modal.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import GlowButton from "../ui/glow-button";
+import { cn } from "@/lib/utils";
+import { X } from "lucide-react";
+import { Button } from "../ui/button";
+
+interface AddCardModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  loading: boolean;
+  setLoading: (isLoading: boolean) => void;
+}
+
+const AddCardModal = ({
+  isOpen,
+  onClose,
+  loading,
+  setLoading,
+}: AddCardModalProps) => {
+  const [isMounted, setisMounted] = useState(false);
+
+  useEffect(() => {
+    setisMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <dialog
+      open={isOpen}
+      className={cn(
+        "fixed left-0 top-0 z-50 flex h-full w-full bg-[#00020A]/80 opacity-100 backdrop-blur-sm transition-all",
+        !isOpen && "-z-50 opacity-0",
+      )}
+    >
+      <div
+        className={cn(
+          "absolute left-1/2 top-1/2 flex h-1/2 w-1/2 -translate-x-1/2 -translate-y-1/2 scale-90 flex-col gap-4 rounded-3xl bg-[#00020A]/90 p-8 text-white opacity-0 shadow-[0px_0px_20px_2px_rgba(89,_29,_169,_0.50)] transition-all duration-200",
+          isOpen && "scale-100 opacity-100",
+        )}
+      >
+        <div className="flex w-full items-center justify-between pb-4">
+          <p className="text-2xl font-bold 2xl:text-3xl">Add Card</p>
+          <button
+            onClick={() => {
+              onClose();
+            }}
+          >
+            <X className="h-6 w-6 text-gray" />
+          </button>
+        </div>
+        <div className="flex h-full flex-col gap-6">
+          <div className="flex rounded-lg border border-gray">
+            <input
+              type="text"
+              placeholder="Enter front text"
+              className="h-fit w-full bg-transparent p-4"
+            />
+          </div>
+          <div className="flex flex-1 rounded-lg border border-gray">
+            <textarea
+              placeholder="Enter back text"
+              className="h-full w-full resize-none bg-transparent p-4 outline-none focus:outline-none"
+            />
+          </div>
+        </div>
+        <div className="flex w-full items-center justify-end gap-4">
+          <Button
+            className="rounded-full bg-transparent p-6"
+            variant={"outline"}
+          >
+            <p>Cancel</p>
+          </Button>
+          <GlowButton showIcon={false}>Add Card</GlowButton>
+        </div>
+      </div>
+    </dialog>
+  );
+};
+
+export default AddCardModal;
