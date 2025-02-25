394d048202ee6a99660bb235ac80fd7dc78dce8a
Ken <johnkennethvelano@gmail.com>
2025-02-25 14:20:48 +0800

    fix merge

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -22,7 +22,6 @@ const RouteLayout = ({ children }: { children: React.ReactNode }) => {
         loading={isLoading}
         setLoading={setIsLoading}
       />
-      <Sidebar />
       <div
         className="m-0 mt-16 flex items-center justify-center p-0 transition-all lg:m-0 lg:ml-auto lg:px-10 lg:py-8"
         style={{
@@ -34,14 +33,6 @@ const RouteLayout = ({ children }: { children: React.ReactNode }) => {
         }}
       >
         <div className="h-full w-full xl:max-w-[calc(100%_-_80px)] 2xl:max-w-[1500px]">
-          {!isOpen && (
-            <button
-              onClick={() => setIsOpen(true)}
-              className="fixed left-[95%] top-[95%] z-50 flex aspect-square -translate-x-[100%] -translate-y-[100%] items-center justify-center rounded-xl bg-primary p-4"
-            >
-              <TimerIcon className="h-8 w-8" />
-            </button>
-          )}
           {children}
         </div>
       </div>
